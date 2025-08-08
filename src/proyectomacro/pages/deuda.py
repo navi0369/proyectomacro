41bd6950260d8b79532c173c228d28feebc2ee17
navi <ajatajuan697@gmail.com>
2025-08-08 14:27:23 -0400

    se terminó de generar el layout para secciones

--- src/proyectomacro/pages/deuda.py
+++ src/proyectomacro/pages/deuda.py
@@ -1,9 +1,24 @@
 # src/proyectomacro/pages/deuda.py
 import dash
 from dash import html
+import dash_bootstrap_components as dbc
+
+from proyectomacro.config import load_pages_config
+from proyectomacro.page_utils import build_section_cards
 
 dash.register_page(__name__, name="Deuda", path="/deuda")
 
-layout = html.Div([
-    html.H3("Deuda"),
-])
+PAGES = load_pages_config()
+sec_cfg = PAGES["deuda"]
+
+tabla_ids = [meta["tabla"] for meta in sec_cfg["tablas"].values()]
+labels = {meta["tabla"]: meta["label"] for meta in sec_cfg["tablas"].values()}
+
+layout = dbc.Container([
+    html.H2(sec_cfg["name"], className="my-4"),
+    build_section_cards(
+        tablas=tabla_ids,
+        labels=labels,
+        base_path=sec_cfg["path"],
+    ),
+], fluid=True)
--- src/proyectomacro/pages/sector_monetario.py
+++ src/proyectomacro/pages/sector_monetario.py
@@ -1,9 +1,24 @@
 # src/proyectomacro/pages/sector_monetario.py
 import dash
 from dash import html
+import dash_bootstrap_components as dbc
+
+from proyectomacro.config import load_pages_config
+from proyectomacro.page_utils import build_section_cards
 
 dash.register_page(__name__, name="Sector Monetario", path="/sector-monetario")
 
-layout = html.Div([
-    html.H3("Sector Monetario"),
-])
+PAGES = load_pages_config()
+sec_cfg = PAGES["sector_monetario"]
+
+tabla_ids = [meta["tabla"] for meta in sec_cfg["tablas"].values()]
+labels = {meta["tabla"]: meta["label"] for meta in sec_cfg["tablas"].values()}
+
+layout = dbc.Container([
+    html.H2(sec_cfg["name"], className="my-4"),
+    build_section_cards(
+        tablas=tabla_ids,
+        labels=labels,
+        base_path=sec_cfg["path"],
+    ),
+], fluid=True)
